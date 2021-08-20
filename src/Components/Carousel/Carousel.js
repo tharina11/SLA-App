3dc0ee42ecfe2fa3980cc94f36e82c723bc499e2
WalukaUI <cwaluka@yahoo.com>
2021-08-19 22:42:34 -0700

    styling

--- src/Components/Carousel/Carousel.js
+++ src/Components/Carousel/Carousel.js
@@ -1,11 +1,9 @@
 import React from "react";
 import { Carousel } from "react-bootstrap";
-import "./Carousel.css"
+import "./Carousel.css";
 
 function Carosel() {
-  return (
-    <>
-    <div className="carousel">
+  return <div className="carousel">
       <Carousel fade>
         <Carousel.Item>
           <img className="d-block w-100" src="../img/1.jpg" alt="First slide" />
@@ -37,8 +35,6 @@ function Carosel() {
         </Carousel.Item>
       </Carousel>
       </div>
-    </>
-  );
 }
 
 export default Carosel;
